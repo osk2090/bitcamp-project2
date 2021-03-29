497bbd28e8beabaf5a3c734b068c8ab4a9139d9d
osk2090 <osk2090@gmail.com>
2021-03-30 01:43:29 +0900

    .

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/TaskAddHandler.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/TaskAddHandler.java
@@ -23,55 +23,55 @@ public class TaskAddHandler implements Command {
   public void service() throws Exception {
     System.out.println("[작업 등록]");
 
-    //현재 등록된 프로젝트 목록을 가져온다
-    List<Project> projects = new ArrayList<>();//그 목록을 한번에 가져올 배열을 만들어준다
+    // 1) 현재 등록된 프로젝트 목록을 가져온다.
+    List<Project> projects = new ArrayList<>();
     try (Connection con = DriverManager.getConnection(
             "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
          PreparedStatement stmt = con.prepareStatement(
-                 "select no,title from oms_project order by title asc");
+                 "select no,title from pms_project order by title asc");
          ResultSet rs = stmt.executeQuery()) {
 
       while (rs.next()) {
         Project p = new Project();
         p.setNo(rs.getInt("no"));
         p.setTitle(rs.getString("title"));
-        projects.add(p);//no와 title을 배열에 담아 넣는다
+        projects.add(p);
       }
 
-      //프로젝트 목록을 출력한다
+
+      // 2) 프로젝트 목록을 출력한다.
       System.out.println("프로젝트들:");
-      if (projects.size() == 0) {//아까 그 배열에 아무것도 없다면
-        System.out.println("현재 등록된 프로젝트가 없습니다.");
+      if (projects.size() == 0) {
+        System.out.println("현재 등록된 프로젝트가 없습니다!");
         return;
       }
-      for (Project p : projects) {//배열에 내용물이 있다면 출력
-        System.out.printf("   %d, %s\n", p.getNo(), p.getTitle());
+      for (Project p : projects) {
+        System.out.printf("  %d, %s\n", p.getNo(), p.getTitle());
       }
 
-      //작업을 등록할 프로젝트를 선택한다
-      int selectProjectNo = 0;
-      loop:
-      while (true) {
+      // 3) 작업을 등록할 프로젝트를 선택한다.
+      int selectedProjectNo = 0;
+      loop: while (true) {
         String input = Prompt.inputString("프로젝트 번호?(취소: 빈 문자열) ");
-        if (input.length() == 0) {//input을 그냥 엔터만 눌렀다면
+        if (input.length() == 0) {
           System.out.println("작업 등록을 취소합니다.");
           return;
         }
-        try {//숫자인지 아닌지 확인하는 구간
-          selectProjectNo = Integer.parseInt(input);
+        try {
+          selectedProjectNo = Integer.parseInt(input);
         } catch (Exception e) {
           System.out.println("숫자를 입력하세요!");
           continue;
         }
         for (Project p : projects) {
-          if (p.getNo() == selectProjectNo) {//가져온 배열의 no와 선택한 번호가 같다면
-            break loop;//while문을 멈춘다
+          if (p.getNo() == selectedProjectNo) {
+            break loop;
           }
         }
-//        System.out.println("유효하지 않은 프로젝트 번호 입니다.");
+        System.out.println("유효하지 않은 프로젝트 번호 입니다.");
       }
 
-      //작업 정보를 입력 받는다
+      // 4) 작업 정보를 입력 받는다.
       Task t = new Task();
       t.setContent(Prompt.inputString("내용? "));
       t.setDeadline(Prompt.inputDate("마감일? "));
@@ -83,15 +83,15 @@ public class TaskAddHandler implements Command {
         return;
       }
 
-      try (PreparedStatement stmt2 = con.prepareStatement(//sql에 데이터를 넣는다
-              "insert into pms_task(content,deadline,owner,status,project_no) values (?,?,?,?,?)")) {
+      try (PreparedStatement stmt2 = con.prepareStatement(
+              "insert into pms_task(content,deadline,owner,status,project_no) values(?,?,?,?,?)");) {
 
-        stmt.setString(1, t.getContent());
-        stmt.setDate(2, t.getDeadline());
-        stmt.setInt(3, t.getOwner().getNo());
-        stmt.setInt(4, t.getStatus());
-        stmt.setInt(5, selectProjectNo);//위에서 입력받은 숫자가 곧 포함되는 프로젝트의 번호이다
-        stmt.executeUpdate();
+        stmt2.setString(1, t.getContent());
+        stmt2.setDate(2, t.getDeadline());
+        stmt2.setInt(3, t.getOwner().getNo());
+        stmt2.setInt(4, t.getStatus());
+        stmt2.setInt(5, selectedProjectNo);
+        stmt2.executeUpdate();
 
         System.out.println("작업을 등록했습니다.");
       }
--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/TaskListHandler.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/TaskListHandler.java
@@ -16,44 +16,45 @@ public class TaskListHandler implements Command {
 
     String input = Prompt.inputString("프로젝트 번호?(전체: 빈 문자열 또는 0) ");
 
-    //사용자가 입력한 문자열을 프로젝트 번호로 바꾼다
+    // 1) 사용자가 입력한 문자열을 프로젝트 번호로 바꾼다.
     int projectNo = 0;
     try {
       if (input.length() != 0) {
         projectNo = Integer.parseInt(input);
       }
-    } catch (Exception e) {
+    }catch (Exception e) {
       System.out.println("프로젝트 번호를 입력하세요.");
       return;
     }
 
-    //해당 프로젝트에 소속된 작업 목록을 가져온다
+    // 2) 해당 프로젝트에 소속된 작업 목록을 가져온다.
     try (Connection con = DriverManager.getConnection(
             "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
-         PreparedStatement stmt = con.prepareStatement(
+         PreparedStatement stmt2 = con.prepareStatement(
                  "select "
-                         + " t.no,"
-                         + " t.content,"
-                         + " t.deadline,"
-                         + " t.status,"
-                         + " m.no as owner_no,"
-                         + " m.name as owner_name"
-                         + " p.no as project_no"
-                         + " p.title as project_title"
+                         + "   t.no,"
+                         + "   t.content,"
+                         + "   t.deadline,"
+                         + "   t.status,"
+                         + "   m.no as owner_no,"
+                         + "   m.name as owner_name,"
+                         + "   p.no as project_no,"
+                         + "   p.title as project_title"
                          + " from pms_task t "
-                         + " inner join pms_member m on t.owner=m.no"
-                         + " inner join pms_project m on t.project_no=p.no"
+                         + "   inner join pms_member m on t.owner=m.no"
+                         + "   inner join pms_project p on t.project_no=p.no"
                          + " where "
-                         + " t.project_no=? or 0=?"
+                         + "   t.project_no=? or 0=?"
                          + " order by p.no desc, t.content asc")) {
-      stmt.setInt(1, projectNo);
-      stmt.setInt(2, projectNo);
 
-      try (ResultSet rs = stmt.executeQuery()) {
+      stmt2.setInt(1, projectNo);
+      stmt2.setInt(2, projectNo);
+
+      try (ResultSet rs = stmt2.executeQuery()) {
         int count = 0;
         while (rs.next()) {
           if (projectNo != rs.getInt("project_no")) {
-            System.out.printf("'%s' 작업 목록: \n", rs.getString("project_no"));
+            System.out.printf("'%s' 작업 목록: \n", rs.getString("project_title"));
             projectNo = rs.getInt("project_no");
           }
           System.out.printf("%d, %s, %s, %s, %s\n",
@@ -71,3 +72,4 @@ public class TaskListHandler implements Command {
     }
   }
 }
+
