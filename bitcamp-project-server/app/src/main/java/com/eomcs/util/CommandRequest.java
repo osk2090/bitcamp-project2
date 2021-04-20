57a8d1a0e5f6bcff44ea5d60c27fda7028a26e2b
osk2090 <osk2090@naver.com>
2021-04-20 12:12:52 +0900

    update

--- bitcamp-project-server/app/src/main/java/com/eomcs/util/CommandRequest.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/util/CommandRequest.java
@@ -5,8 +5,10 @@ public class CommandRequest {
     private String commandPath;
     private String remoteAddr;
     private int remotePort;
+    private Prompt prompt;
 
-    public CommandRequest(String commandPath, String remoteAddr, int remotePort) {
+    public CommandRequest(String commandPath, String remoteAddr, int remotePort, Prompt prompt) {
+        this.prompt = prompt;
         this.remotePort = remotePort;
         this.commandPath = commandPath;
         this.remoteAddr = remoteAddr;
@@ -22,5 +24,9 @@ public class CommandRequest {
 
     public int getRemotePort() {
         return remotePort;
+    }
+
+    public Prompt getPrompt() {
+        return prompt;
     }
 }
