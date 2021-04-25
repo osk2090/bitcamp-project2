6a59b1bae99cf31d3f4d3976c013599ee45b452e
unknown <osk2090@naver.com>
2021-04-25 23:26:34 +0900

    28-b

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
@@ -9,8 +9,6 @@ import java.net.Socket;
 
 public class ClientApp {
 
-  String sessionId;
-
   public static void main(String[] args) {
     ClientApp app = new ClientApp();
     try {
@@ -56,7 +54,7 @@ public class ClientApp {
     String command = input.substring(i); // => /board/list
 
     // => 사용자가 입력한 문자열에서 서버 주소와 포트 번호를 분리하여 추출한다.
-    String[] values = input.substring(0, i).split(":"); // => {"192.168.0.2", "8888"} 
+    String[] values = input.substring(0, i).split(":"); // => {"192.168.0.2", "8888"}
     String serverAddress = values[0]; // => "192.168.0.2"
     int port = 8888;
     if (values.length > 1) {
@@ -64,37 +62,14 @@ public class ClientApp {
     }
 
     try (Socket socket = new Socket(serverAddress, port);
-        PrintWriter out = new PrintWriter(socket.getOutputStream());
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        ) {
+         PrintWriter out = new PrintWriter(socket.getOutputStream());
+         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    ) {
 
       // 서버에게 명령을 보낸다.
       out.println(command);
-      if (sessionId != null) {
-        out.printf("SESSION_ID:%s\n", sessionId);
-      }
       out.println();
       out.flush();
-
-      // 서버가 보내온 응답 헤더를 읽는다.
-      // => 상태 값을 읽는다.
-      in.readLine(); // 상태 값은 당장 사용하지는 않는다.
-
-      // => 응답 헤더를 읽는다.
-      while (true) {
-        String line = in.readLine();
-        if (line.length() == 0) {
-          break;
-        }
-
-        // 서버가 세션 아이디를 보내 주면 보관해 뒀다가,
-        // 서버에 요청할 때마다 요청 헤더로 다시 보내 준다.
-        // 왜?
-        // 클라이언트를 구분할 수 있도록 하기 위함이다.
-        if (line.startsWith("SESSION_ID:")) {
-          sessionId = line.substring(11);
-        }
-      }
 
       // 서버가 응답한 데이터를 출력한다.
       String line = null;
